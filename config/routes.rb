43df325adf840363d3b3dc7308ec6036c2910cb4
ethanchen <ethan@fresho.com>
2024-07-03 11:06:32 +1000

    demo about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'marketplace#index'
+  get :about , to: 'marketplace#about'
   resources :products, only: [:index]
   resources :cart, only: [:update]
   get "up" => "rails/health#show", as: :rails_health_check
