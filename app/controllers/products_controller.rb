04b5e7bd9063fb8a2a4d21386b04d53f7ef57fb9
ethanchen <ethan@fresho.com>
2024-07-03 10:35:07 +1000

    products controller

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,11 @@
+class ProductsController < ApplicationController
+  before_action :authenticate_user!
+  def index
+    @products = if params[:name] 
+                  Product.where("name ILIKE ?", "%#{params[:name]}%")
+                else
+                  Product.all
+                end
+    @cart = current_user.cart || Cart.create(user: current_user)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'marketplace#index'
+  resources :products, only: [:index]
   resources :cart, only: [:update]
   get "up" => "rails/health#show", as: :rails_health_check
 end
